f2262de1a2bd60a42a5d26c3112d4f59c9c5857e
Sandesh Soni <sandeshsoni1@gmail.com>
2022-04-04 22:42:24 +0530

    convert content to type text, considering Latex

--- apps/dakghar/priv/repo/migrations/20220403184330_create_cards.exs
+++ apps/dakghar/priv/repo/migrations/20220403184330_create_cards.exs
@@ -5,8 +5,9 @@ defmodule Dakghar.Repo.Migrations.CreateCards do
     create table(:flashcards, primary_key: false) do
       add :id, :binary_id, primary_key: true
       add :topic_code, :string, null: false
-      add :front_content, :string, null: false
-      add :back_content, :string, null: false
+      add :front_content, :text, null: false
+      add :back_content, :text, null: false
+      add :hint, :text, null: false
       add :sequence_no, :integer, null: false
       add :deck_id, references(:flashcard_decks, type: :binary_id, on_delete: :delete_all)
       timestamps()
