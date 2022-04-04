75c0cddc98d1f0ad08030568e71724cfb5346d9d
Sandesh Soni <sandeshsoni1@gmail.com>
2022-04-04 10:13:26 +0530

    modify some view. and update corresponding test

--- apps/dakghar_web/test/dakghar_web/controllers/page_controller_test.exs
+++ apps/dakghar_web/test/dakghar_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule DakgharWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to Study"
   end
 end
